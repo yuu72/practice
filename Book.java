0a274eb7e8499cc1f795c7652768a1786dec4f1a
yuu72 <connect.postbox.h23@gmail.com>
2022-01-13 10:34:52 +0900

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,9 @@
+public class Book etends TangibleAssent{
+	private String isbn;
+	public Book(String name.int price,String color,String isbn){
+		super(name,price,color);
+		this.isbn = isbn;
+	}
+	public String getlsbn(){return this.isbn;}
+}
+
